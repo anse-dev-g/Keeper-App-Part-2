fa56e340888f8f730dfa34bdf8502894ffea1845
anse-dev-g <79371836+anse-dev-g@users.noreply.github.com>
2021-07-20 17:24:53 -0400

    Adding more rendering props, mapping and arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      <dl>
+        {notes.map((newNotes) => (
+          <Note
+            key={newNotes.key}
+            title={newNotes.title}
+            content={newNotes.content}
+          />
+        ))}
+      </dl>
       <Footer />
     </div>
   );
